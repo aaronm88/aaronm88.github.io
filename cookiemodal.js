a06a1195f5cb1abe71abb0fd7501dfe55cfce208
aaronm88 <aaron.d.mcgarvey@gmail.com>
2023-05-12 10:20:17 -0400

    Added cookie policy and modal

---
+++ cookiemodal.js
@@ -0,0 +1,7 @@
+const cookieModal = document.querySelector('.cookie__modal')
+const cookieModalButton = document.querySelector('.section-button__cookiemodal')
+console.log(cookieModalButton)
+
+cookieModalButton.addEventListener('click', function() {
+    cookieModal.style.display = "none";
+    });
